f7d69e5137ddb7a9c5d37538a07dcae614b1b8ac
illm4nn3rd <63482035+illm4nn3rd@users.noreply.github.com>
2023-12-22 17:01:45 -0800

    Update pdn.js

--- recipes/img/pdn.js
+++ recipes/img/pdn.js
@@ -1,1 +1,25 @@
+// generator.js
+
+function generatePentadecimal() {
+    const length = parseInt(document.getElementById('length').value);
+    let pentadecimalNumber = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomDigit = getRandomDigit();
+        pentadecimalNumber += randomDigit;
+    }
+
+    displayResult(pentadecimalNumber);
+}
+
+function getRandomDigit() {
+    const symbols = ['L', '2', '9', 'w', 'd', 'u', 'q', '0', 'p', 'n', 'b', 'm', '5', '6', '7'];
+    const randomIndex = Math.floor(Math.random() * symbols.length);
+    return symbols[randomIndex];
+}
+
+function displayResult(result) {
+    const resultContainer = document.getElementById('result');
+    resultContainer.textContent = `Generated Pentadecimal Number: ${result}`;
+}
 
